0172cfc56039f5488a7acb036d62094006a9b9fb
Giorgos Konstas <gio.konstas@hotmial.com>
2025-05-02 12:18:30 +0300

    svg creation. changed secondaryBtn to glowing border

--- js/script.js
+++ js/script.js
@@ -6,21 +6,3 @@ window.addEventListener("scroll", () => {
     ? navbar.classList.add("navbar-scroll")
     : navbar.classList.remove("navbar-scroll");
 });
-
-// Glow effect on button
-document.querySelectorAll(".secondaryBtn").forEach((button) => {
-  // Set default glow position to the right end
-  button.style.setProperty("--x", "100%");
-
-  button.addEventListener("mousemove", (e) => {
-    const rect = button.getBoundingClientRect();
-    const x = e.clientX - rect.left;
-    const y = e.clientY - rect.top;
-    button.style.setProperty("--x", `${x}px`);
-  });
-
-  button.addEventListener("mouseleave", () => {
-    // Reset to default position when mouse leaves
-    button.style.setProperty("--x", "90%");
-  });
-});
